6c9501c0046c605ebcf05418ed64ddb06e2a6401
Mona Lisa <email@example.com>
2020-09-10 20:09:19 -0600

    starting

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -9,37 +10,62 @@ namespace ChallengesWithTestsMark8
         {
             return Char.IsLetter(c);
 
-            //if (CharacterIsALetter == )
-            //{
-            //    return true;
-
-            //}
-            //else
-            //{
-            //    return false;
-            //}
-
-            //throw new NotImplementedException();
+         
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+            
+                
+            
+            //throw new NotImplementedException();
         }
 
         public bool IsNumberEven(int number)
+
         {
-            throw new NotImplementedException();
+            return number % 2 == 0;
+           
+
+            //throw new NotImplementedException();
         }
+
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return num % 2 != 0;
+            //throw new NotImplementedException();
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+
+            
+            
+                if (numbers == null || numbers.Count() == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    var min = numbers.Min();
+                    var max = numbers.Max();
+                    return min + max;
+                }
+            
+
+
+
+            //throw new NotImplementedException();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
@@ -49,7 +75,23 @@ namespace ChallengesWithTestsMark8
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            var total = 0;
+
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+
+            foreach (var item in numbers)
+            {
+
+                total += item;
+            }
+            return total; 
+
+
+            //throw new NotImplementedException();
         }
 
         public int SumEvens(int[] numbers)
@@ -64,7 +106,27 @@ namespace ChallengesWithTestsMark8
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+
+            if (number < 0)
+            {
+                return 0;
+            }
+            return number / 2;
+            //throw new NotImplementedException();
         }
+
+
+        
+
+ 
+
+
+            
+        
+
     }
+
+
+
 }
+
