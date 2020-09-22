c9cd14b310967ed5b5449352170fc7188614a712
Mona Lisa <email@example.com>
2020-09-21 19:47:13 -0600

    starting

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -21,10 +22,6 @@ namespace ChallengesWithTestsMark8
             }
             return false;
         }
-
-
-
-
 
 
 
@@ -51,25 +48,54 @@ namespace ChallengesWithTestsMark8
             return ((sum % 2) != 0);
         }
 
+
+
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            if (password == null)
+            {
+                return false;
+            }
+
+            var isUpper = password.Any(c => char.IsUpper(c));
+
+            var isLower = password.Any(c => char.IsLower(c));
+
+            var isNumber = password.Any(c => char.IsNumber(c));
+
+            return isLower && isNumber && isUpper;
+            
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return val[val.Length-1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
-        }
+            //throw new NotImplementedException();
+
+            if (divisor == 0)
+            {
+                return 0;
+            }
+
+                decimal result = 0;
+
+                result = dividend / divisor;
+
+                return result;
+        } 
 
         public int LastMinusFirst(int[] nums)
         {
